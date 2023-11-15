531d770dda174c2de3761199924928e044e2b319
solarkdexter <dexterb@sol-ark.com>
2023-11-15 17:03:17 -0600

    L2 finished

---
+++ tests/unit/AppHeader.spec.js
@@ -0,0 +1,18 @@
+import { mount } from '@vue/test-utils'
+import AppHeader from '@/components/AppHeader'
+
+describe('AppHeader', () => {
+  test('if user is not logged in, do not show logout button', () => {
+    const wrapper = mount(AppHeader)
+    expect(wrapper.find('button').isVisible()).toBe(false)
+  })
+  // it() is an alias for test()
+
+  test('if a user is logged in, show logout button', async () => {
+    const wrapper = mount(AppHeader)
+    wrapper.setData({ loggedIn: true })
+
+    await wrapper.vm.$nextTick()
+    expect(wrapper.find('button').isVisible()).toBe(true)
+  })
+})
